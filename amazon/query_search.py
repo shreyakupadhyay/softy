5dc09e811afdf93147c710a9daee9cfbe8f4ae63
Raghav Bhatnagar <raghavbhatnagar96@gmail.com>
2016-09-11 01:36:37 +0530

    Made it multithreading instead of multiprocessing. Returns a dictionary with all the products and their costs.

--- amazon/query_search.py
+++ amazon/query_search.py
@@ -9,6 +9,8 @@ Getting data from amazon on the basis of query search by the user.
 import requests
 from bs4 import BeautifulSoup
 import multiprocessing
+from threading import Thread
+finalDict = {}
 
 def AmazonScrape(url):
     s = requests.session()
@@ -18,81 +20,37 @@ def AmazonScrape(url):
     soup = BeautifulSoup(data)
     allItems = soup.findAll('div', {'class': 's-item-container'})
     for item in allItems:
-        print item.find('h2').contents[0]
+        title = item.find('h2').contents[0]
         price = item.find('span', {'class': 'a-size-base a-color-price s-price a-text-bold'})
         if price==None:
-            print 'offers from: Rs.'+item.find('a', {'class': 'a-size-small a-link-normal a-text-normal'}).findAll('span', {'class': 'a-color-price'})[1].contents[-1].encode('ascii')
+            price = item.find('a', {'class': 'a-size-small a-link-normal a-text-normal'}).findAll('span', {'class': 'a-color-price'})[1].contents[-1].encode('utf-8')
         else:
-            print 'Rs. ' + price.contents[1].encode('ascii')
-        print '...........'
-    print 'END OF FUNC '+ url[-1]+' ...........................................................'
+            price = price.contents[1].encode('utf-8')
+        title = title.encode('utf-8')
+        #print title
+        finalDict[title]=('amazon', price)
 
-searchFor = raw_input("what do you want to search for?  ")
-params = searchFor.split(" ")
-url = 'http://www.amazon.in/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords='+searchFor
-urlArray = []
-for i in range(1,4):
-     urlArray.append(url+'&page='+str(i))
-#print urlArray
-pool = multiprocessing.Pool(processes=3) # how much parallelism?
-pool.map(AmazonScrape, urlArray)
+#searchFor = raw_input("what do you want to search for?  ")
+def AmazonGet(search_param):
+    url = 'http://www.amazon.in/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords='+search_param
+    urlArray = []
+    for i in range(1,4):
+        urlArray.append(url+'&page='+str(i))
+    threadlist=[]
+    t1 = Thread(target=AmazonScrape ,args=(urlArray[0],))
+    t1.start()
+    threadlist.append(t1)
+    t2 = Thread(target=AmazonScrape ,args=(urlArray[1],))
+    t2.start()
+    threadlist.append(t2)
+    t3 = Thread(target=AmazonScrape ,args=(urlArray[2],))
+    t3.start()
+    threadlist.append(t3)   
+    # print finalDict
+    t1.join()
+    t2.join()
+    t3.join()
+    return finalDict
 
 
-
-
-
-
-
-
-
-
-
-# p.start()
-# p.join()
-# q = multiprocessing.Process(target=AmazonScrape, args=(url, 2))
-# q.start()
-# q.join()
-# for eachparam in params:
-#     url = url + eachparam + "+"
-#url2= 'http://www.google.com'
-#print url
-# s = requests.session()
-# headers= {'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/45.0.2454.101 Chrome/45.0.2454.101 Safari/537.36'}
-# page = s.get(url,headers=headers)
-# data = page.text
-# soup = BeautifulSoup(data)
-# #print 'souping done'
-# allItems = soup.findAll('div', {'class': 's-item-container'})
-# for item in allItems:
-#     print item.find('h2').contents[0]
-#     price = item.find('span', {'class': 'a-size-base a-color-price s-price a-text-bold'})
-#     if price==None:
-#         print 'offers from: Rs.'+item.find('a', {'class': 'a-size-small a-link-normal a-text-normal'}).findAll('span', {'class': 'a-color-price'})[1].contents[-1].encode('ascii')
-#     else:
-#         print 'Rs. ' + price.contents[1].encode('ascii')
-#     print '...........'
-# url2 = url+'&page=2'
-# page = s.get(url2,headers=headers)
-# data = page.text
-# soup = BeautifulSoup(data)
-# #print 'souping done'
-# allItems = soup.findAll('div', {'class': 's-item-container'})
-# for item in allItems:
-#     print item.find('h2').contents[0]
-#     price = item.find('span', {'class': 'a-size-base a-color-price s-price a-text-bold'})
-#     if price==None:
-#         print 'offers from: Rs.'+item.find('a', {'class': 'a-size-small a-link-normal a-text-normal'}).findAll('span', {'class': 'a-color-price'})[1].contents[-1].encode('ascii')
-#     else:
-#         print 'Rs. ' + price.contents[1].encode('ascii')
-#     print '...........'
-
-#for link in soup.find_all('a'):
-#   print(link.get('href'))
-# mydivs = soup.findAll("a", { "class" : "a-link-normal s-access-detail-page  a-text-normal" })
-# myprice = soup.findAll("span", { "class" : "a-size-base a-color-price s-price a-text-bold" })
-# #print mydivs[0]['title']
-# print myprice[0]
-# print soup.title
-#print mydivs
-#raw_input()
-#print soup
+#AmazonGet('iphone') To use, use AmazonGet
